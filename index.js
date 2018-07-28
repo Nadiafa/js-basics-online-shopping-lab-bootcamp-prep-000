8725db202e458cb88ea54a18ebc192c61d76b2cc
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-28 07:56:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,43 +21,45 @@
       }
     }
 
-function viewCart() {
-  return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
-  var emptyWording = "Your shopping cart is empty."
-  var oneItemCart = "In your cart, you have "
-  var moreItemsCart = " "
-  if (getCart().length === 0) {
-    return emptyWording;
-  } else if (getCart().length >= 1) {
-    oneItemCart = oneItemCart + `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
-    return oneItemCart;
-  } else if (getCart().length >= 2) {
-    for (var i=1; i<getCart().length -1; i++) {
-      moreItemsCart = moreItemsCart + `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+    function viewCart() {
+      return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
+      var emptyWording = "Your shopping cart is empty."
+      var oneItemCart = "In your cart, you have "
+      var moreItemsCart = " "
+      if (getCart().length === 0) {
+        return emptyWording;
+      } else if (getCart().length >= 1) {
+        oneItemCart = oneItemCart + `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
+        return oneItemCart;
+      } else if (getCart().length >= 2) {
+        for (var i=1; i<getCart().length -1; i++) {
+          moreItemsCart = moreItemsCart + `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+        }
+        oneItemCart = oneItemCart +`${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
+      }
+      return `${oneItemCart}.`
     }
-    oneItemCart = oneItemCart +`${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
-  }
-  return `${oneItemCart}.`
-}
-
-
-
-
-
 
 
 
 function total() {
-  var sum = sumUpPrices()
-  return sum
-}
-function sumUpPrices() {
   var sum = 0
   for (var i=0; i<getCart().length; i++) {
     sum = sum + getCart()[i].itemPrice
   }
-  return sum
 }
+
+// function total() {
+//   var sum = sumUpPrices()
+//   return sum
+// }
+// function sumUpPrices() {
+//   var sum = 0
+//   for (var i=0; i<getCart().length; i++) {
+//     sum = sum + getCart()[i].itemPrice
+//   }
+//   return sum
+// }
 
 function removeFromCart(itemName) {
   var itemToRemove = searchCartForItemToRemove(itemName)
