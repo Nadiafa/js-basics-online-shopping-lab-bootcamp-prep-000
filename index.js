6bab9e69ba2ce527d503e84f077dd3c43dad4320
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-28 07:22:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,73 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ // write your code 
+ var item = cartItems(item);
+ getCart().push(item);
+ return `${item.itemName} has been added to your cart.`;
+
+
+}
+//function for object / addToCart
+function cartItems(itemName) {
+    return {
+      itemName: itemName,
+      itemPrice: Math.floor(Math.random() * 100)
+    };
 }
 
-function viewCart() {
+
+
+
+function generateCartDescription() {
+  var cartDescription = 'In your cart, you have '
+  if ( getCart().length >= 1 ) {
+    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`}
+  if ( getCart().length >= 2 ) {
+    var middleCartItemsDescription = ''
+    for (var i=1; i<getCart().length -1; i++) {
+      middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`}
+    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`}
+  return `${cartDescription}.`}
+
+
+
+function viewCart() { 
   // write your code here
+  if (getCart().length === 0) {
+    return `Your shopping cart is empty.`;
+  } else if () {
+      var wording = `In your cart, you have `;
+      if (getCart().length = 1) {
+        return wording + `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`;
+  } else if (getCart().length = 2) {
+      var wordingForTwo = ` `;
+      for (var i=1; i<getCart().length -1; i++) {
+        wordingForTwo = wordingForTwo+`, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+      }
+  wording = wording + `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function viewCart() {
+  return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
 }
+
 
 function total() {
   // write your code here
