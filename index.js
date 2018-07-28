25e64b1aa98ad58fbca98244b90d3182ba4b65d7
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-28 07:40:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,87 +1,91 @@
-    var cart = [];
-    
-    function getCart() {
-     return cart;
-    }
-    
-    function setCart(c) {
-      cart = c;
-      return cart;
-    }
+var cart = [];
+
+function getCart() {
+ return cart;
+}
+
+function setCart(c) {
+  cart = c;
+  return cart;
+}
 
 function addToCart(item) {
-  var item = newCartItem(item)
-  getCart().push(item)
-  return `${item.itemName} has been added to your cart.`
+ // write your code 
+ var item = cartItems(item);
+ getCart().push(item);
+ return `${item.itemName} has been added to your cart.`;
+
+
 }
-function newCartItem(itemName) {
-  return {
-    itemName:itemName,
-    itemPrice:getRandomInt(1, 100)
-  }
+//function for object / addToCart
+function cartItems(itemName) {
+    return {
+      itemName: itemName,
+      itemPrice: Math.floor(Math.random() * 100)
+    };
 }
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+
+
+
+
+function generateCartDescription() {
+  var cartDescription = 'In your cart, you have '
+  if ( getCart().length >= 1 ) {
+    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`}
+  if ( getCart().length >= 2 ) {
+    var middleCartItemsDescription = ''
+    for (var i=1; i<getCart().length -1; i++) {
+      middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`}
+    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`}
+  return `${cartDescription}.`}
+
+
+
+function viewCart() { 
+  // write your code here
+  if (getCart().length === 0) {
+    return `Your shopping cart is empty.`;
+  } else if () {
+      var wording = `In your cart, you have `;
+      if (getCart().length = 1) {
+        return wording + `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`;
+  } else if (getCart().length = 2) {
+      var wordingForTwo = ` `;
+      for (var i=1; i<getCart().length -1; i++) {
+        wordingForTwo = wordingForTwo+`, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+      }
+  wording = wording + `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function viewCart() {
   return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
 }
-function generateCartDescription() {
-  var cartDescription = 'In your cart, you have '
-  if ( getCart().length >= 1 ) {
-    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
-  }
-  if ( getCart().length >= 2 ) {
-    var middleCartItemsDescription = ''
-    for (var i=1; i<getCart().length -1; i++) {
-      middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
-    }
-    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
-  }
 
-  return `${cartDescription}.`
-}
 
 function total() {
-  var sum = sumUpPrices()
-  return sum
-}
-function sumUpPrices() {
-  var sum = 0
-  for (var i=0; i<getCart().length; i++) {
-    sum = sum + getCart()[i].itemPrice
-  }
-  return sum
+  // write your code here
 }
 
-function removeFromCart(itemName) {
-  var itemToRemove = searchCartForItemToRemove(itemName)
-  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
-}
-function searchCartForItemToRemove(itemName) {
-  var searchResult
-  for (var i=0; i<getCart().length; i++) {
-    if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
-  }
-  return searchResult
-}
-function removeItemFromCart(itemToRemove) {
-  var indexOfItemToRemove = cart.indexOf(itemToRemove)
-  //Array.prototype.splice()
-  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
-  getCart().splice(indexOfItemToRemove,1)
-}
-function notifyUserThereIsNoItemToRemove() {
-  return 'That item is not in your cart.'
+function removeFromCart(item) {
+  // write your code here
 }
 
 function placeOrder(cardNumber) {
-  if (arguments[0] == undefined) {
-    return "Sorry, we don't have a credit card on file for you."
-  } else {
-    var sumToCharge = total()
-    setCart([])
-    return `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`
-  }
+  // write your code here
 }
